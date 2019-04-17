e8cb14810c388beaf8443ac4a81d17c6f2abe72e
Nicolas <n.leray94@gmail.com>
2019-04-17 21:59:08 +0200

    yolo

---
+++ Assets/Scripts/CursorCylinder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CursorCylinder : MonoBehaviour
+{
+	public Cursor c;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.localScale = new Vector3(c.RadiusLivingDetection, transform.localScale.y, c.RadiusLivingDetection);
+    }
+}
