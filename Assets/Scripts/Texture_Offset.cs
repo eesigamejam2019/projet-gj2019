f339065bdc24ae731d36253cd6c9d1199a33ded6
CLeN624 <clementsipion@gmail.com>
2019-04-17 22:21:21 +0200

    le feu du feu du feu

---
+++ Assets/Scripts/Texture_Offset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texture_Offset : MonoBehaviour
+{ // Scroll main texture based on time
+
+  public  float YScroll = 1.0f;
+    public float XScroll = 1.0f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float Xoffset = Time.time * XScroll;
+        float Yoffset = Time.time * YScroll;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(Xoffset, Yoffset));
+    }
+}
