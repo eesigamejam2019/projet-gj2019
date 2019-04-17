1e5ba65ebd1d933ed9e92924bcd2c8b15a2d67b6
Nicolas <n.leray94@gmail.com>
2019-04-17 15:39:37 +0200

    scene test

---
+++ Assets/Scripts/SpriteFaceCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SpriteFaceCamera : MonoBehaviour
+{
+	private Camera cam;
+	// Use this for initialization
+	void Start()
+	{
+		cam = Camera.main;
+	}
+
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		transform.rotation= cam.transform.rotation;
+	}
+}
