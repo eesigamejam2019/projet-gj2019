d5a4126a4d0e6559c5165170ae2413c6eb4da8e6
Paul <defois.p@laposte.net>
2019-04-18 15:15:34 +0200

    LifeSlider

--- Assets/Scripts/LifeSlider.cs
+++ Assets/Scripts/LifeSlider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeSlider : MonoBehaviour
 {
@@ -8,28 +9,41 @@ public class LifeSlider : MonoBehaviour
     private int treeCount;
     private int treeAlive;
 
+    public Slider slider;
+
     // Start is called before the first frame update
     void Start()
     {
         arbres = GameObject.FindObjectsOfType<LivingItem>();
         foreach (LivingItem item in arbres)
         {
-            if (item.isEndLife())
+            if (item.isFullLife())
             {
                 treeAlive++;
             }
         }
+        treeCount = arbres.Length;
+        UpdateSlider();
+    }
+
+    private void OnEnable()
+    {
         LivingItem.OnChangeLife += ChangeLife;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-
+        LivingItem.OnChangeLife -= ChangeLife;
     }
 
-    void ChangeLife(int value)
+    private void ChangeLife(int value)
     {
         treeAlive += value;
+        UpdateSlider();
+    }
+
+    private void UpdateSlider()
+    {
+        slider.value = (float)treeAlive / (float)treeCount;
     }
 }
