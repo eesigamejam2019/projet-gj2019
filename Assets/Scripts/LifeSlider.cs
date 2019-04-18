3d206d9b670033c5063c540502b512008a81b9d0
Paul <defois.p@laposte.net>
2019-04-18 14:05:49 +0200

    lifeslider

---
+++ Assets/Scripts/LifeSlider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeSlider : MonoBehaviour
+{
+    private LivingItem[] arbres;
+    private int treeCount;
+    private int treeAlive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arbres = GameObject.FindObjectsOfType<LivingItem>();
+        foreach (LivingItem item in arbres)
+        {
+            if (item.isEndLife())
+            {
+                treeAlive++;
+            }
+        }
+        LivingItem.OnChangeLife += ChangeLife;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ChangeLife(int value)
+    {
+        treeAlive += value;
+    }
+}
